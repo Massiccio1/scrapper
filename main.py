6bf71b5ea6141e7e0953ae111039ea49d2313035
Massiccio1 <girardellimassimo@gmail.com>
2024-02-14 17:55:40 +0100

    checkpoint

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from googlesearch import search
 headerList = ['Azienda', 'link1', 'link2', 'link3']
   
 # open CSV file and assign header 
-with open("parsed.csv", 'w') as file: 
-    dw = csv.DictWriter(file, delimiter=',',  
-                        fieldnames=headerList) 
-    dw.writeheader() 
+# with open("parsed.csv", 'w') as file: 
+#     dw = csv.DictWriter(file, delimiter=',',  
+#                         fieldnames=headerList) 
+#     dw.writeheader() 
 
 
 with open('nomi_aziende.csv', 'r') as csvfile:
@@ -23,8 +23,8 @@ with open('nomi_aziende.csv', 'r') as csvfile:
     next(reader, None)
     
     
-    with open('parsed.html', 'a') as f:
-            f.write("<table border=\"1\">")
+    # with open('parsed.html', 'a') as f:
+    #         f.write("<table border=\"1\">")
             
     
 
@@ -41,7 +41,7 @@ with open('nomi_aziende.csv', 'r') as csvfile:
         
         row=[azienda]
         
-        for url in search(query, num=1, stop=3):
+        for url in search(query, num=1, stop=3, pause=1):
             # Aggiungere l'URL alla riga
             print(url)
             row.append(url)
